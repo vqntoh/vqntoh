cd3c2f39447b62e4c5b18f86788f21f123f3dc0c
vqntoh <130864284+vqntoh@users.noreply.github.com>
2024-04-18 02:23:12 +0200

    Update README.java

--- README.java
+++ README.java
@@ -1,8 +1,12 @@
 public class Profile{
+  String name;
+  int age;
+  String country;
+  String[] languages;
 public Profile(){
-  this.name = vqntoh;
+  this.name = "vqntoh";
   this.age = 19;
   this.country = "Italy";
-  this.languages = new String[]{"Java", "JS", "C", "Python"};
+  this.languages = new String[]{"Java", "JS", "C", "C++", "Python"};
   }
 }
